2f8fdacb44e22c9325969aab6f95887e41f748ba
Bryan Daniel <brdaniel117@gmail.com>
2021-08-08 18:23:29 -0700

    reviewed comment.js added note

--- models/Comment.js
+++ models/Comment.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+//create our comment models
 class Comment extends Model {}
 
 Comment.init(
