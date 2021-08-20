4d497289019bfbcf7e6ab9e07f647a88e7c21a6b
Thomas Begush <tbegush@gmail.com>
2021-08-19 19:04:29 -0700

    added comments to explain the code better

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: { maxAge: 60000 },
+  cookie: { },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
